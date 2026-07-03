0a39e061a4fd8810178c96beeff0bb93f5148ff1
Poorna Kumar Ravipati <poorna.kumar1433@gmail.com>
2026-07-03 18:29:09 +0530

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello from CICD-Pipeline-Kubernetes");
+  res.send("Hello from CICD-Pipeline-Kubernetes with 2 replicas");
 });
 
 app.listen(3000, "0.0.0.0", () => {
